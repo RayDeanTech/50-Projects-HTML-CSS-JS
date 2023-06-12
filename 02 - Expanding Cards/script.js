6ee90d14b8c775617586d9b41ea5ea4ff9506bfd
Raymond Dean <RayDeanTech@outlook.com>
2023-06-12 11:48:58 -0400

    commit Project 2 - Expanding Cards

---
+++ 02 - Expanding Cards/script.js
@@ -0,0 +1,20 @@
+const panels = document.querySelectorAll('.panel');  //object of panels
+
+
+panels.forEach((panel) => {
+
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        panel.classList.add('active');
+    });
+
+});
+
+
+function removeActiveClasses() {
+
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    })
+
+};
